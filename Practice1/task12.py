0003d7c839f9810a5864912f9e4debcc9ef3537a
MertveZI <ss2013105@yandex.ru>
2026-02-11 21:16:18 +0700

    taktak update
    Solved:
    task12
    Added:
    task14
    task15

--- Practice1/task12.py
+++ Practice1/task12.py
@@ -4,25 +4,24 @@ import numpy as np
 np.random.seed(42)
 
 # Time index
-dates = pd.date_range('2025-01-01', periods=1000, freq='H')  # 1000 часов
+dates = pd.date_range('2025-01-01', periods=1000, freq='h')  # 1000 часов
 df = pd.DataFrame(index=dates)
 
 # Value
 t_hours = (df.index - df.index[0]).total_seconds() / 3600
-df['value'] = ___
+df['value'] = np.sin(2 * np.pi * t_hours / 24) + np.random.normal(0, 0.1, len(df)) + 0.001 * t_hours
 
 # Category
-df['category'] = np.random.choice(['___', '___', '___'], len(df))
+df['category'] = np.random.choice(['A', 'B', 'C'], len(df))
 
 # Resample
-daily = df['value'].resample('___').agg(['___', '___', '___']) 
+daily = df['value'].resample('d').agg(['mean', 'max', 'min']) 
 print("Daily stats:")
 print(daily.head())
 
 # Rolling
-df['rolling_mean'] = df['value'].rolling(window=___).___()  # 7 дней = сколько часов?
-df['rolling_std'] = df['value'].rolling(window=___).___()
-
+df['rolling_mean'] = df['value'].rolling(window=168).mean()  # 7 дней = сколько часов?
+df['rolling_std'] = df['value'].rolling(window=168).std()
 # Max volatility
 max_vol_day = df['rolling_std'].idxmax()
 print("День с максимальной волатильностью:", max_vol_day)
